a977023ad6a5664f35f4b5b788f176b59a460ec2
noreplydev <contact.cristiansanchez@gmail.com>
2023-07-27 19:27:09 +0200

    [FIX] Now the nested object are well rendered

--- components/build/Product.tsx
+++ components/build/Product.tsx
@@ -25,18 +25,49 @@ function ObjectRenderer(object: Object, level: number): any{
   const keys = Object.keys(object)
   if (keys.length === 0) return null
 
+  // each key is a property of the object
   return keys.map((key, index)=> {
     const depthLevel = findDeepestLevel(object[key as keyof typeof object])  
 
-    const array = Object.values(object[key as keyof typeof object])
-    return <div key={index} style={{ marginLeft: indenting+'px'}}
-      className={`flex ${depthLevel < 2? 'flex-row items-center justify-start' 
-      : 'flex-col items-start justify-start'} h-fit w-full px-5`}>
-      <div className="flex flex-row justify-start items-center">
-        {level === 1 && <div className="h-1 w-1 bg-black rounded-sm mr-2"></div>}
-        <p className="text-base text-black font-light">{key}:</p>
+    if (Array.isArray(object[key as keyof typeof object])) {
+      const array = Object.values(object[key as keyof typeof object])
+
+      return <div key={index} style={{ marginLeft: indenting+'px'}}
+        className={`flex ${depthLevel < 2? 'flex-row items-center justify-start' 
+        : 'flex-col items-start justify-start'} h-fit w-full px-5`}>
+        <div className="flex flex-row justify-start items-center">
+          {level === 1 && <div className="h-1 w-1 bg-black rounded-sm mr-2"></div>}
+          <p className="text-base text-black font-light">{key}:</p>
+        </div>
+        {ArrayRenderer(array, level)}
       </div>
-      {ArrayRenderer(array, level)}
+    }
+
+    if (object[key as keyof typeof object] instanceof Object) {
+      const obj = object[key as keyof typeof object] as Object
+
+      // conditionally render a column or a row
+      return <div className={`flex ${depthLevel > 1? 'flex-row items-center justify-start' 
+        : 'flex-col items-start justify-start'} h-fit w-full px-5`}
+        key={JSON.stringify(obj[key as keyof typeof obj])}
+        >
+        <div className="flex flex-row h-fit w-fit items-center justify-start">
+        <div className="h-1 w-1 bg-black rounded-sm mr-2"></div>
+        <p className="text-base text-black font-light">{key}:</p>
+        </div>
+        {ObjectRenderer(obj, level++)}
+      </div>
+    }
+
+    const string = object[key as keyof typeof object] as any
+
+    // conditionally render a column or a row
+    return <div className={`flex ${depthLevel < 2? 'flex-row items-center justify-start' 
+      : 'flex-col items-start justify-start'} h-fit w-full px-5`}
+      key={JSON.stringify(object[key as keyof typeof object])}
+    >
+        <p className="text-base text-black font-light">{key}:</p>
+        {StringRenderer(string, level++)}
     </div>
   })
 }
@@ -56,9 +87,9 @@ function ArrayRenderer(array: Array<any>, level: number): any{
 function StringRenderer(string: string, level: number): any {
   const indenting = level * 2
 
-  return <p className={`text-base text-[#0056ff] font-light 
-    ${indenting ? 'px-'+indenting : ''}`}
-  >{string}</p>
+  return <p className={`text-base ${string ? 'text-[#0056ff]' : 'text-[#898989]'} font-light 
+    ${indenting ? 'px-2' : ''}`}
+  >{string || 'undefined'}</p>
 }
 
 // Main function rendering object properties
@@ -71,7 +102,6 @@ function renderProduct(keys: Array<string>, data: any, level: number): any{
     if (data[key] instanceof Object)  renderType = 'object'
     if (Array.isArray(data[key])) renderType = 'array'
 
-    console.log(renderType)
     const depthLevel = findDeepestLevel(data[key])
 
     // conditionally render a column or a row
