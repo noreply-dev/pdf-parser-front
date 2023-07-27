da9256eaff5ac9d41f73ccf47b06c63be07e48a1
noreplydev <contact.cristiansanchez@gmail.com>
2023-07-27 17:25:48 +0200

    [FT] Products viewer added to the panel

--- components/build/Product.tsx
+++ components/build/Product.tsx
@@ -1,41 +1,106 @@
 'use client'
 
-function ArrayToString(array: Array<any>): string {
-  array.forEach(item => {
-    if (item instanceof Object) return ArrayToString(item)
-  }) 
-  return array.join(',')
+const productsRenderers = {
+  'array': ArrayRenderer,
+  'object': ObjectRenderer,
+  'string': StringRenderer
 }
 
-function renderChild(keys: Array<string>, data: any, count: number): any{
-  return keys.map((key, index) => {
-    // Product title
-    if (key === 'name') return <h3 className="text-base font-medium" key={index}>{data[key]}</h3>
-    
-    // Render arrays
-    if (data[key] instanceof Array) return <div>
-      <p className={`${'px-'+(count*2)} text-base font-light`}>{key}: {ArrayToString(data[key])}</p> 
-    </div>
-    
-    // Rended objects
-    if (data[key] instanceof Object) return renderChild(Object.keys(data[key]), data[key], count++)
+function findDeepestLevel(object: Object): number {
+  let deepestLevel = 0
 
-    return <div key={key}>
-      <p className={`text-sm font-light text-[#898989] 
-        ${'px-'+(count*2)}`}
-      >{key}: {data[key]}</p> 
+  Object.keys(object).forEach(key => {
+    if (object[key as keyof typeof object] instanceof Object) {
+      const level = findDeepestLevel(object[key as keyof typeof object])
+      if (level > deepestLevel) deepestLevel = level
+    }
+  })
+
+  return deepestLevel + 1
+}
+
+// Object renderer the most complex renderer
+function ObjectRenderer(object: Object, level: number): any{
+  const indenting = level * 15
+  const keys = Object.keys(object)
+  if (keys.length === 0) return null
+
+  return keys.map((key, index)=> {
+    const depthLevel = findDeepestLevel(object[key as keyof typeof object])  
+
+    if (Array.isArray(object[key as keyof typeof object])) {
+      const array = Object.values(object[key as keyof typeof object])
+      return <div key={index} style={{ marginLeft: indenting+'px'}}
+        className={`flex ${depthLevel < 2? 'flex-row items-center justify-start' 
+        : 'flex-col items-start justify-start'} h-fit w-full px-5`}>
+        <div className="flex flex-row justify-start items-center">
+          {level === 1 && <div className="h-1 w-1 bg-black rounded-sm mr-2"></div>}
+          <p className="text-base text-black font-light">{key}:</p>
+        </div>
+        {ArrayRenderer(array, level)}
+      </div>
+    }
+  })
+}
+
+// Array is based on other renderers and its depth
+function ArrayRenderer(array: Array<any>, level: number): any{
+  const depthLevel = findDeepestLevel(array)
+
+  if (depthLevel > 1) {
+    return ObjectRenderer(Object.assign({}, array), level++)
+  }
+
+  return StringRenderer(array.join(', '), level)
+}
+
+// The smallest renderable unit
+function StringRenderer(string: string, level: number): any {
+  const indenting = level * 2
+
+  return <p className={`text-base text-[#0056ff] font-light 
+    ${indenting ? 'px-'+indenting : ''}`}
+  >{string}</p>
+}
+
+// Main function rendering object properties
+function renderProduct(keys: Array<string>, data: any, level: number): any{
+  // Ignore the name property
+  data.name ? keys.splice(keys.indexOf('name'), 1) : null
+
+  return keys.map((key, index) => {
+    let renderType = 'string'
+    if (data[key] instanceof Object)  renderType = 'object'
+    if (Array.isArray(data[key])) renderType = 'array'
+
+    const depthLevel = findDeepestLevel(data[key])
+
+    // conditionally render a column or a row
+    return <div className={`flex ${renderType === 'string' || depthLevel < 2? 'flex-row items-center justify-start' 
+        : 'flex-col items-start justify-start'} h-fit w-full px-5`}
+        key={JSON.stringify(data[key])}
+      >
+      <div className="flex flex-row h-fit w-fit items-center justify-start">
+        <div className="h-1.5 w-1.5 bg-black rounded-sm mr-2"></div>
+        <p className="text-base text-black font-light">{key}:</p>
+      </div>
+      {productsRenderers[renderType as keyof typeof productsRenderers](data[key], level)}
     </div>
   })
 }
 
 export function Product({page, index}: {page: any, index: number}) {
   let pageN = index+1
-  console.log(page)
+
   return <div className="flex flex-col w-full h-fit gap-3">
     {
       page.map((product: any, index: number) => {
-        return <div className="bg-[#ececec] px-5 py-4 rounded-md" key={index}>
-          {renderChild(Object.keys(product), product, 1)}
+        const productKeys = Object.keys(product)
+        const indentingLevel = 1
+
+        return <div className="flex flex-col bg-[#ececec] px-5 py-4 rounded-md" key={index}>
+          {product?.name && <h3 className="text-base font-medium mb-2">{product.name}</h3>}
+          {renderProduct(productKeys, product, indentingLevel)}
         </div>
       })
     } 
